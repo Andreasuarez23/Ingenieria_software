22eec724e5bf137961434aaca0579f4a5a1adfbf
Mijaliv <130709522+Mijaliv@users.noreply.github.com>
2024-07-03 21:10:45 -0300

    clase reporte

---
+++ entities_library/report/Report.cs
@@ -0,0 +1,19 @@
+using entities_library.login;
+
+namespace entities_library.report;
+
+public class Report {
+    
+    public long Id { get; set;}
+
+    public required User User { get; set; }
+
+    public required ReportStatus ReportStatus { get; set;}
+
+    public required DateTime DateTime { get; set; }
+
+    public required DateTime DateTime { get; set; }
+
+    public required string Reason { get; set; }
+
+}
